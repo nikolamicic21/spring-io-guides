a4753759060aecb8796258610f5396aa8d624450
mickeckemi21 <mickeandroid21@gmail.com>
2021-01-16 20:13:39 +0100

    Integrating Data Tests

--- integrating-data/src/test/kotlin/io/mickeckemi21/springioguides/integratingdata/IntegratingDataApplicationTests.kt
+++ integrating-data/src/test/kotlin/io/mickeckemi21/springioguides/integratingdata/IntegratingDataApplicationTests.kt
@@ -1,13 +1,58 @@
 package io.mickeckemi21.springioguides.integratingdata
 
+import com.rometools.rome.feed.synd.SyndEntryImpl
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.integration.endpoint.SourcePollingChannelAdapter
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.support.MessageBuilder
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 
-@SpringBootTest
+@SpringBootTest(
+    properties = [
+        "feed.auto-startup=false",
+        "feed.file.name=Test"
+    ]
+)
 class IntegratingDataApplicationTests {
 
+    @Autowired
+    private lateinit var newsAdapter: SourcePollingChannelAdapter
+
+    @Autowired
+    private lateinit var news: MessageChannel
+
     @Test
-    fun contextLoads() {
+    fun `integration flow feed - file test`() {
+        assertFalse(newsAdapter.isRunning)
+
+        val testTitle = "Test Title"
+        val testLink = "http://characters/frodo"
+        val syndEntry = SyndEntryImpl().apply {
+            title = testTitle
+            link = testLink
+        }
+
+        val fileOut = File("tmp/si/Test")
+        fileOut.delete()
+
+        assertFalse(fileOut.exists())
+
+        news.send(MessageBuilder.withPayload(syndEntry).build())
+
+        assertTrue(fileOut.exists())
+
+        val reader = BufferedReader(FileReader(fileOut))
+        val line = reader.readLine()
+
+        assertEquals("$testTitle @ $testLink", line)
+
+        reader.close()
+        fileOut.delete()
     }
 
 }
