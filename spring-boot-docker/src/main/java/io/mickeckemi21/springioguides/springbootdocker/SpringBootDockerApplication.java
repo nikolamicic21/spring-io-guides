5dac037449dd374d60fc9810c18e0f3af3657a85
mickeckemi21 <mickeandroid21@gmail.com>
2021-01-23 23:30:16 +0100

    Spring Boot Docker

---
+++ spring-boot-docker/src/main/java/io/mickeckemi21/springioguides/springbootdocker/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package io.mickeckemi21.springioguides.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Docker World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
+
+}
